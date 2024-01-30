4e9e4db8f757f0b919ed64092ef793d43c96a960
haruyma8940 <yaruo0406@gmail.com>
2024-01-31 03:44:47 +0900

    add sort

--- scripts/topo.py
+++ scripts/topo.py
@@ -12,10 +12,14 @@ type_color_dict = {"straight_road":"blue", "dead_end":"lime", "corner":"green","
                 #    , "corner", "cross_road", "3way_right", "3way_center", "3way_left"]
 # color_list = ["blue", "lime", "blueviolet", "green", "gold", "aqua", "red", "orange"]
 
+
+def check_overlapping(pos1,pos2 ,min_dist=0.1):
+    return np.linalg.norm(np.array(pos1) - np.array(pos2)) < min_dist
+
 # グラフを作成
 G = nx.Graph()
 pos = {}  # ノードの位置を格納する辞書
-edge_length = 1  # すべてのエッジの長さを一定とする
+edge_length = 2  # すべてのエッジの長さを一定とする
 
 # 初期ノードの位置を設定
 pos[1] = (0, 0)  # 最初のノードの位置を原点とする
@@ -51,12 +55,11 @@ for edge_id, nodes in edge_to_nodes.items():
     # for edge_id1, edge_id2  in edge_to_nodes[edge_id]:
             # print(edge_in_node_id1,edge_in_node_id2)
             G.add_edge(edge_in_node_id1,edge_in_node_id2,id=edge_id)
-
+            edge_labels[(edge_in_node_id1, edge_in_node_id2)] = str(edge_id)
 
 
 # pos[1] = (0.0)
 pos = {1: (0,0)} # The position of the initial node is the origin.
-
 for (node_id,edge_id),deg in  edge_info_dict.items():
     # print(pos)
     if node_id in pos: #nodeの位置を現在のnodeをもとに決定
@@ -64,42 +67,21 @@ for (node_id,edge_id),deg in  edge_info_dict.items():
         for other_node_id in edge_to_nodes[edge_id]:
             if node_id != other_node_id and other_node_id not in pos:
                 pos[other_node_id] = (
-                                    pos[node_id][0] + edge_length * np.cos(rad),
-                                    pos[node_id][1] + edge_length * np.sin(rad)
+                                    round((pos[node_id][0] + edge_length * np.cos(rad)),1 ),
+                                    round((pos[node_id][1] + edge_length * np.sin(rad)),1)
                                     )
 pos2 = dict(sorted(pos.items()))
 print(pos)
 print(pos2)
-# for node_id,(pos_x,pos_y) in pos.items():
-#     print(node_id)
+pos_id = 1
+# for node_pos_id ,pos_node_id in pos2.items():
 
-#     for edge_info in data['topomap']:
-#         if 'node' in other_item and other_item['node']['id'] != node_id:
-#             other_node_data = other_item['node']
-#             if other_node_id not in pos:
-
-
-#     for other_item in G.nodes:
-#         if other_node_id != node_id and G.has_edge(node_id,other_node_id):
-#                 if 'node' in other_item and other_item['node']['id'] != node_id:
-#                     other_node_data = other_item['node']
-#                     if any(edge['id'] == edge_id for edge in other_node_data['edge']):
-#                         other_node_id = other_node_data['id']
-#                         if other_node_id not in pos:
-#                             # 隣接ノードの位置を計算
-#                             pos[other_node_id] = (
-#                                 pos[node_id][0] + edge_length * np.cos(rad),
-#                                 pos[node_id][1] + edge_length * np.sin(rad)
-#                             )
-# #                         # エッジを追加し、エッジIDを属性として設定
-# #                         G.add_edge(node_id, other_node_id, id=edge_id)
-# #                         # エッジラベルの設定
-# #                         edge_labels[(node_id, other_node_id)] = str(edge_id)
+    
 node_colors = [type_color_dict[G.nodes[node]["type"]] for node in G.nodes()]
 # # # グラフを描画
 # nx.draw(G, pos, with_labels=True, node_size=800, edge_color='gray', node_color=node_colors, font_size=12)
 nx.draw(G, pos2,with_labels=True, node_size=800, node_color=node_colors,edge_color='gray', font_size=12)
 # # # エッジラベルを描画
-# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
+nx.draw_networkx_edge_labels(G, pos2, edge_labels=edge_labels, font_color='blue')
 
 plt.show()
